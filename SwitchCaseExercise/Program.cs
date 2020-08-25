086e142c1d6171cc51bbea31ac8cdb594aa398a8
Brian H <humphreybrian58@gmail.com>
2020-08-25 13:57:10 -0400

    Finalized Switch case exercise

---
+++ SwitchCaseExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwitchCaseExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch/Case Exercise Start");
+            Console.WriteLine("What is your favoririte subject in school?");
+            var answer = Console.ReadLine().ToLower();
+
+            switch (answer)
+            {
+                case "math":
+                    Console.WriteLine("That's my favorite subject");
+                    break;
+                case "science":
+                    Console.WriteLine("That's a pretty fun subject");
+                    break;
+                case "gym":
+                    Console.WriteLine("Everybody loves gym!!!");
+                    break;
+                case "history":
+                    Console.WriteLine("Who doesn't love history");
+                    break;
+                case "english":
+                    Console.WriteLine("I hated english class");
+                    break;
+                default:
+                    Console.WriteLine($"I am unfamiliar with {answer} class");
+                    break;
+            }
+        }
+    }
+}
